4daa7fef14b4ef0769f1acfae4bcbddfdf04eb4e
Eva J Herzog <ejherzog@users.noreply.github.com>
2017-02-25 12:41:10 -0500

    updated locations

--- Main.java
+++ Main.java
@@ -3,24 +3,31 @@ import java.util.ArrayList;
 public class Main {
 	ArrayList<Location> allLocs = new ArrayList<Location>();
 	public Main(){
-		allLocs.add(new Location(39.9532281, -75.1589546, "Reading Terminal Market"));
-		allLocs.add(new Location(39.949531,-75.149732, "Independence National Historic Park"));
+		allLocs.add(new Location( 39.9532281, -75.1589546, "Reading Terminal Market"));
+		allLocs.add(new Location( 39.949531,-75.149732, "Independence National Historic Park"));
+		allLocs.add(new Location( 39.96557,-75.180966, "Philadelphia Museum of Art"));
+		allLocs.add(new Location( 39.94961,-75.150282, "Liberty Bell Center"));
+		allLocs.add(new Location( 39.948874,-75.150023, "Independence Hall"));
+		allLocs.add(new Location( 39.96493,-75.180047, "Rocky Steps"));
+		allLocs.add(new Location( 39.953892,-75.149066, "National Constituion Center"));
+		allLocs.add(new Location( 40.100145,-75.430196, "Valley Forge National Historic Park"));
+		allLocs.add(new Location( 39.953853,-75.165294 , "LOVE Park"));
+		allLocs.add(new Location( 39.955809,-75.150411, "Franklin Square"));
+		allLocs.add(new Location( 39.958211,-75.173135, "Franklin Institute"));
+		allLocs.add(new Location( 39.973911,-75.19481, "Philadelphia Zoo"));
+		allLocs.add(new Location( 39.949239,-75.191367, "Penn Museum of Archaeology and Anthropology"));
+		allLocs.add(new Location( 39.968336,-75.172665, "Eastern State Penitentiary"));
+		allLocs.add(new Location( 39.949529,-75.146888, "Benjamin Franklin Museum"));
+		allLocs.add(new Location( 39.950181,-75.148426, "National Museum of American Jewish History"));
+		allLocs.add(new Location( 39.950849,-75.150161, "Independence Visitor Center"));
+		allLocs.add(new Location( 39.953311,-75.176639, "Mutter Museum"));
+		allLocs.add(new Location( 39.952195,-75.180653, "Schuylkill Banks"));
+		allLocs.add(new Location( 39.952767,-75.142397, "Elfreth's Alley"));
+		allLocs.add(new Location( 39.953304,-75.151765, "African American Museum of Philadelphia"));
+		allLocs.add(new Location( 39.950261,-75.151843, "Philadelphia History Museum"));
+		allLocs.add(new Location( 39.963741,-75.132385, "SugarHouse Casino"));
 	}
 	
-	
-	
-	Location independencePark = new Location(39.949531,-75.149732, "Independence National Historic Park");
-	Location pma = new Location(39.96557,-75.180966, "Philadelphia Museum of Art");
-	Location libertyBell = new Location(39.94961,-75.150282, "Liberty Bell Center");
-	Location indHall = new Location( 39.948874,-75.150023, "Independence Hall");
-	Location rockySteps = new Location( 39.96493,-75.180047, "Rocky Steps");
-	Location natConstitutionCenter = new Location( 39.953892,-75.149066, "National Constituion Center");
-	Location valleyForge = new Location( 40.100145,-75.430196, "Valley Forge National Historic Park");
-	Location lovePark = new Location( 39.953853,-75.165294 , "LOVE Park");
-
-	Location franklinSq = new Location(39.955809,-75.150411, "Franklin Square");
-	Location franklinInstitute = new Location( 39.958211,-75.173135, "Franklin Institute");
-	Location zoo = new Location(39.973911,-75.19481, "Philadelphia Zoo");
 	
 
 }
