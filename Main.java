84dfbe7ac4965326b32fa1389d88d6ff6d38f904
rrehman93 <rrehman@seas.upenn.edu>
2017-02-25 12:48:47 -0500

    Update Main.java
    added locations

--- Main.java
+++ Main.java
@@ -15,6 +15,12 @@ public class Main {
 		allLocs.add(new Location( 39.955809,-75.150411, "Franklin Square"));
 		allLocs.add(new Location( 39.958211,-75.173135, "Franklin Institute"));
 		allLocs.add(new Location( 39.973911,-75.19481, "Philadelphia Zoo"));
+		allLocs.add(new Location(new Location(39.960643,-75.172807, "Barnes Foundation")));
+		allLocs.add(new Location(new Location(39.95247,-75.167847, "One Liberty Observation Deck")));
+		allLocs.add(new Location(new Location(39.952302,-75.144746, "The Betsy Ross House")));
+		allLocs.add(new Location(new Location(39.979487,-75.209012, "Please Touch Museum")));
+		allLocs.add(new Location(new Location(39.955608,-75.163143, "Pennsylvania Academy of the Fine Arts")));
+		allLocs.add(new Location(new Location(39.95709,-75.171205, "Academy of Natural Sciences")));
 		allLocs.add(new Location( 39.949239,-75.191367, "Penn Museum of Archaeology and Anthropology"));
 		allLocs.add(new Location( 39.968336,-75.172665, "Eastern State Penitentiary"));
 		allLocs.add(new Location( 39.949529,-75.146888, "Benjamin Franklin Museum"));
