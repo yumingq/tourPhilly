5d77b0a9c36d48a304e3f98432bdb3058c4169a5
Yuming Qin <yumingq@wharton.upenn.edu>
2017-02-25 12:38:39 -0500

    new main activity

---
+++ app/src/main/java/com/yumingqin/walkphilly/MainActivity.java
@@ -0,0 +1,46 @@
+package com.yumingqin.walkphilly;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    Spinner spinner;
+    ArrayAdapter<CharSequence> adapter;
+    static CharSequence location;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+        adapter = ArrayAdapter.createFromResource(this, R.array.locations, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                location = (CharSequence) parent.getItemAtPosition(position);
+                int numLocs = Integer.parseInt((String) location);
+                System.out.println(numLocs);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    public void onLaunchButtonClick(View view){
+        Intent i = new Intent(this, MapsActivity.class);
+        startActivity(i);
+    }
+}
